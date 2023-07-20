22e6a68a52261b606dc49acd31233e6f26348321
Glitch (glitch-hello-website) <none>
2023-07-20 03:52:46 +0000

    💄🚈 Checkpoint ./requirements.txt:60050747/39 ./app.py:60050747/225 ./templates/index.html:60050747/118

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import *
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+app = flask(__name__)
+
+
+@app.route('/')
+def index
+
+
+if __name__ == '__main__':
+  app.run(debug = "true")
