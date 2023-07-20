a4a1702c1a7943fc3abaec13ae7f9ccf875b8a37
Glitch (glitch-hello-website) <none>
2023-07-20 15:26:02 +0000

    📅⛪️ Checkpoint ./app.py:60050747/470

--- app.py
+++ app.py
@@ -7,10 +7,22 @@ app = flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template()
+  return render_template('index.html')
 
+def predict():
+  return render_template('')
 
-
+@app.route('/cp',methods = ['POST'])
+def housepricepredict():
+  
+  BHK = eval(request.form.get("BHK"))
+  Bathroom = eval(request.form.get("Bathroom"))
+  Parking = eval(request.form.get("Parking"))
+  Per_Sqft = eval(request.form.get("Per_Sqft"))
+  Furnishing = eval(request.form.get("Furnishing"))
+  
+  url = "delhi property price for ml project1.csv"
+  df = pd.read_csv(url)
 
 
 
