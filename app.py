4c90ee4caa59232babed14a5bc29403f7de57686
Glitch (glitch-hello-website) <none>
2023-07-22 07:27:14 +0000

    🏏🍰 Checkpoint ./app.py:57629255/1057 ./requirements.txt:57629255/58 ./delhi_house_price_ML.csv:57629255/3 ./start.sh:57629255/20

--- app.py
+++ app.py
@@ -1,27 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 
-app = flask(__name__)
+app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-  return render_template('index.html')
+def home():
+    return render_template("index.html")
 
-@app.route('/predict' methods = [post])
 
+@app.route("/predict", methods=["POST"])
 def predict():
-  BHK = eval(request.form.get("BHK"))
-  Bathroom = eval(request.form.get("Bathroom"))
-  Parking = eval(request.form.get("Parking"))
-  Per_Sqft = eval(request.form.get("Per_Sqft"))
-  Furnishing = eval(request.form.get("Furnishing"))
-  
-   return render_template('predict.html')
-
-@app.route('/cp',methods = ['POST'])
-def housepricepredict():
   
   BHK = eval(request.form.get("BHK"))
   Bathroom = eval(request.form.get("Bathroom"))
@@ -29,19 +19,18 @@ def housepricepredict():
   Per_Sqft = eval(request.form.get("Per_Sqft"))
   Furnishing = eval(request.form.get("Furnishing"))
   
-  url = "delhi house price ML.csv"
+  url = "delhi_house_price_ML.csv"
   data = pd.read_csv(url)
-  X = data.drop(["Price"],axis = "columns")
+  X = data.drop(['id', 'Price'], axis='columns')
   y = data["Price"]
   
   model = LinearRegression()
   model.fit(X,y)
   
-  newdata = model.predict([[BHK, Bathroom, Parking, Per_sqft, Furnishing]])
-  result = arr
+  newdata = model.predict([[BHK, Bathroom, Parking, Per_Sqft, Furnishing]])
   
   return render_template("predict.html", data1 = newdata[0])
 
 
 if __name__ == '__main__':
-  app.run(debug = "true")
+    app.run(debug=True)
